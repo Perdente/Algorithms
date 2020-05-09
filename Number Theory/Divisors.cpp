8027721ca2511400b4ceaca0e0c7be070d4f5adc
Perdente <58706003+Perdente@users.noreply.github.com>
2020-05-09 11:41:22 +0600

    Update Divisors.cpp

--- Number Theory/Divisors.cpp
+++ Number Theory/Divisors.cpp
@@ -7,11 +7,14 @@ void divisor(long long n)
  	vector<long long>v;
  	for(int i=1;i*i<=n;++i)
  	{
- 		if(n/i==i)v.push_back(i);
- 		else 
+ 		if(n%i==0)
  		{
- 			v.push_back(i);
- 			v.push_back(n/i);
+ 			if(n/i==i)v.push_back(i);
+	 		else 
+	 		{
+	 			v.push_back(i);
+	 			v.push_back(n/i);
+			}	
 		}
 	}
 	for(auto i:v)cout<<i<<" ";
