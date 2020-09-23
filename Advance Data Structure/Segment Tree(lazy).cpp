d61f1ac5878c7e65f3df05038c3054346ef09309
Perdente <58706003+Perdente@users.noreply.github.com>
2020-09-23 09:34:10 +0600

    Update Segment Tree(lazy).cpp

--- Advance Data Structure/Segment Tree(lazy).cpp
+++ Advance Data Structure/Segment Tree(lazy).cpp
@@ -1,66 +1,72 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define mx 10005
+
 int num[mx];
 int seg[4*mx];
 int lazy[4*mx];
-void bld_segtree(int pos,int low,int hi)
+
+void bld_segtree(int seg_si,int seg_str,int seg_end)
 {
-	if(low==hi)
+	if(seg_str==seg_end)
 	{
-		seg[pos]=num[low];return ;
+		seg[seg_si]=num[seg_str];return ;
 	}
-	int mid=(low + hi)/2;
-	bld_segtree(2*pos+1,low,mid);//recurse on the left child
-	bld_segtree(2*pos+2,mid+1,hi);//recurse on the right child
-	seg[pos]=min(seg[2*pos+1],seg[2*pos+2]);
+	int mid=(seg_str + seg_end)/2;
+	bld_segtree(2*seg_si+1,seg_str,mid);//recurse on the left child
+	bld_segtree(2*seg_si+2,mid+1,seg_end);//recurse on the right child
+	seg[seg_si]=min(seg[2*seg_si+1],seg[2*seg_si+2]);
 }
-int query(int pos,int L,int R,int low,int hi)
+
+int query(int seg_si,int seg_str,int seg_end,int q_str,int q_end)
 {
-	if(lazy[pos]!=0)
+	if(lazy[seg_si]!=0)
 	{
-		seg[pos]+=lazy[pos];
-		if(L!=R)
+		seg[seg_si]+=lazy[seg_si];
+		if(seg_str!=seg_end)
 		{
-			lazy[2*pos+1]+=lazy[pos];
-			lazy[2*pos+2]+=lazy[pos];
+			lazy[2*seg_si+1]+=lazy[seg_si];
+			lazy[2*seg_si+2]+=lazy[seg_si];
 		}
-		lazy[pos]=0;
+		lazy[seg_si]=0;
 	}
-	if(L>hi or R<low)return INT_MAX;
-	if(L>=low and R<=hi)return seg[pos];
-	int mid=(L+R)/2;
-	int x=query(2*pos+1,L,mid,low,hi);
-	int y=query(2*pos+2,mid+1,R,low,hi);
+	if(seg_str>q_end or seg_end<q_str)return INT_MAX;
+	if(seg_str>=q_str and seg_end<=q_end)return seg[seg_si];
+	int mid=(seg_str+seg_end)/2;
+	int x=query(2*seg_si+1,seg_str,mid,q_str,q_end);
+	int y=query(2*seg_si+2,mid+1,seg_end,q_str,q_end);
 	return min(x,y);
 }
-void update(int at,int L,int R,int l,int r,int value)
+
+void update(int seg_si,int seg_str,int seg_end,int q_str,int q_end,int value)
 {
-	if(lazy[at]!=0)//if lazy contains previous value
+	if(lazy[seg_si]!=0)//if lazy contains previous value
 	{
-		seg[at]+=lazy[at];
-		if(L!=R)
+		seg[seg_si]+=lazy[seg_si];
+		if(seg_str!=seg_end)
 		{
-			lazy[2*at+1]+=lazy[at];
-			lazy[2*at+2]+=lazy[at];
+			lazy[2*seg_si+1]+=lazy[seg_si];
+			lazy[2*seg_si+2]+=lazy[seg_si];
 		}
+		lazy[seg_si]=0;
 	}
-	if(L>r or R<l) return ;//no overlap
-	if(L>=l and R<=r)//total overlap
+	if(seg_str>q_end or seg_end<q_str) return ;//no overlap
+	if(seg_str>=q_str and seg_end<=q_end)//total overlap
 	{
-		seg[at]+=value;//update the range
-		if(L!=R)//if not leaf not then update its children
+		seg[seg_si]+=value;//update the range
+		if(seg_str!=seg_end)//if not leaf not then update its children
 		{
-			lazy[2*at+1]+=value;
-			lazy[2*at+2]+=value;
+			lazy[2*seg_si+1]+=value;
+			lazy[2*seg_si+2]+=value;
 		}
 		return ;
 	}
-	int mid=(L+R)/2;
-	update(2*at+1,L,mid,l,r,value);
-	update(2*at+2,mid+1,R,l,r,value);
-	seg[at]=min(seg[2*at+1],seg[2*at+2]);
+	int mid=(seg_str+seg_end)/2;
+	update(2*seg_si+1,seg_str,mid,q_str,q_end,value);
+	update(2*seg_si+2,mid+1,seg_end,q_str,q_end,value);
+	seg[seg_si]=min(seg[2*seg_si+1],seg[2*seg_si+2]);
 }
+
 int main()
 {
 	int n;cin>>n;
@@ -70,17 +76,17 @@ int main()
 	}
 	memset(lazy,0,sizeof(lazy));
 	bld_segtree(0,0,n-1);//from root = 0 we construct from index 0 to index 3 segment tree in arr_min[]
-	for(int i=0;i<4*n;++i)
-	{
-		cout<<seg[i]<<" ";
-	}
-	cout<<endl;
+//	for(int i=0;i<4*n;++i)
+//	{
+//		cout<<seg[i]<<" ";
+//	}
+//	cout<<endl;
 	cout<<query(0,0,n-1,1,2)<<endl;
 	update(0,0,n-1,0,1,2);
-	for(int i=0;i<4*n;++i)
-	{
-		cout<<seg[i]<<" ";
-	}
+	//for(int i=0;i<4*n;++i)
+	//{
+	//	cout<<seg[i]<<" ";
+	//}
 	cout<<endl;
 	cout<<query(0,0,n-1,0,n-1)<<endl;
 }
